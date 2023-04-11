5db8c4199ade8d80c452a8e7792c0b6f00ae71ce
Fernando Perez <fdoiv7@gmail.com>
2023-04-11 17:31:16 -0600

    Initial Commit

---
+++ MovieDBSwiftUI/MovieDBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBSwiftUIApp.swift
+//  MovieDBSwiftUI
+//
+//  Created by Fernando Ives on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
